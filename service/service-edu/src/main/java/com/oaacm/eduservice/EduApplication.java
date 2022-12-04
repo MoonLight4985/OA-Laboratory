82b072d20c1385b05b900aa8006e975fa64b3a97
moonlight <szy601870112@gmail.com>
2022-12-04 23:06:40 +0800

    Swagger配置测试

--- service/service-edu/src/main/java/com/oaacm/eduservice/EduApplication.java
+++ service/service-edu/src/main/java/com/oaacm/eduservice/EduApplication.java
@@ -3,10 +3,12 @@ package com.oaacm.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = {"com.oaacm"})
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
